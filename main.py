74d6336625bc6f3896026ffe805632006d4b408b
Shane McDonough <shanemcdman@gmail.com>
2020-07-17 15:32:09 -0700

    add another test

--- main.py
+++ main.py
@@ -25,9 +25,15 @@ class BinaryConverterTest(unittest.TestCase):
         for i in range(10000):
             self.assertEqual(BinaryConverter.base10_to_binary(i), int(bin(i).replace("0b", "")))
 
+    def test_binary_to_base10(self):
+        dct = {}
+        for i in range(100):
+            dct[BinaryConverter.base10_to_binary(i)] = i
+        for i in [0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010, 1011, 1100, 1101, 1110, 1111]:
+            self.assertEqual(BinaryConverter.binary_to_base10(i), dct[i])
+
 def main():
-    # unittest.main()
-    print(BinaryConverter.binary_to_base10(11111111))
+    unittest.main()
 
 if __name__ == "__main__":
     main()
