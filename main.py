1cd0f7d595fa34421934dbcf7bb78b1ea4d900a7
Shane McDonough <shanemcdman@gmail.com>
2020-07-17 14:54:39 -0700

    Create base10_to_binary

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+class BinaryConverter:
+
+    @staticmethod
+    def base10_to_binary(num: int) -> int:
+        arr = []
+        while True:
+            arr.insert(0, num % 2)
+            num //= 2
+            if num <= 0:
+                break
+        return int("".join([str(i) for i in arr]))
+
 def main():
-    print("Hello World!")
+    for i in range(256):
+        print(BinaryConverter.base10_to_binary(i))
 
 if __name__ == "__main__":
     main()
